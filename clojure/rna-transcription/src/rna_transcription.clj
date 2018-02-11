f81d75ad6e9f653b9cac5dd8759d68e7f7db5920
Yen-Chin Lee <coldnew.tw@gmail.com>
2018-02-11 14:29:01 +0800

    rna_transcription: add my solution
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,5 +1,13 @@
 (ns rna-transcription)
 
-(defn to-rna [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn- to-rna-1 [c]
+  (case (str c)
+    "C" "G"
+    "G" "C"
+    "A" "U"
+    "T" "A"
+    (throw (AssertionError. (str "Wrong rna code: " c)))))
+
+(defn to-rna [s]
+  (reduce str
+          (map to-rna-1 (seq s))))
